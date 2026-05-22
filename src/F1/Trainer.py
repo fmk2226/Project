054aeb5424796b9b751f0b3fcfa274a8765269fc
fmk2226 <jerryfu0723@gmail.com>
2026-05-22 02:35:06 -0400

    fixed the Trainer and added K-fold & best model & predict to csv

--- src/F1/Trainer.py
+++ src/F1/Trainer.py
@@ -1,18 +1,23 @@
+import pandas as pd
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
+from torch.utils.data import TensorDataset, DataLoader
 import copy
 class Trainer:
-    def __init__(self,net,train_iter,test_iter,num_epochs,lr,optimizer=None,loss=None,device=None):
+    def __init__(self,net,train_feature,train_label,test_feature,batch_size,lr,num_epochs,weight_decay,optimizer=None,loss=None,device=None):
         self.net=net
-        self.train_iter=train_iter
-        self.test_iter=test_iter
+        self.train_feature=torch.tensor(train_feature.values,dtype=torch.float32)
+        self.train_label=torch.tensor(train_label.values,dtype=torch.long).reshape(-1)
+        self.test_feature=torch.tensor(test_feature.values,dtype=torch.float32)
         self.num_epochs=num_epochs
+        self.batch_size=batch_size
         self.lr=lr
+        self.weight_decay=weight_decay
         if optimizer is None:
-            self.optimizer=torch.optim.SGD(self.net.parameters(),lr=self.lr)
+            self.optimizer=torch.optim.SGD(self.net.parameters(),lr=self.lr,weight_decay=self.weight_decay)
         else:
-            self.optimizer=optimizer(self.net.parameters(),lr=self.lr)
+            self.optimizer=optimizer(self.net.parameters(),lr=self.lr,weight_decay=self.weight_decay)
         if loss is None:
             self.loss=nn.CrossEntropyLoss()
         else:
@@ -24,25 +29,47 @@ class Trainer:
         self.net.to(self.device)
         self.history={
             'train_loss':[],
-            'train_mse':[],
-            'test_loss':[],
-            'test_mse':[]
+            'train_acc':[],
+            'val_loss':[],
+            'val_acc':[],
         }
-        self.best_score=float('inf')
+        self.best_score=0.0
         self.best_epoch=0
         self.best_model_state= None
+        self.initial_model_state=copy.deepcopy(self.net.state_dict())
+        self.optimizer_class=torch.optim.SGD if optimizer is None else optimizer
         
-    def MSE(self,y_hat,y):
-        squared_error=((y_hat-y)**2).sum().item()
-        return squared_error
+    def accuracy(self,y_hat,y):
+        y_hat=y_hat.argmax(dim=1)
+        count=(y_hat.type(y.dtype)==y).sum().item()
+        return count
 
-    def train(self):
+    def train(self,X_train,y_train,X_valid,y_valid):
+        self.history = {
+            'train_loss': [],
+            'train_acc': [],
+            'val_loss':[],
+            'val_acc':[],
+            'test_loss': [],
+            'test_acc': []
+        }
+        train_iter=DataLoader(
+            TensorDataset(X_train,y_train),
+            batch_size=self.batch_size,
+            shuffle=True
+        )
+        val_iter=DataLoader(
+            TensorDataset(X_valid,y_valid),
+            batch_size=self.batch_size,
+            shuffle=False
+        )
+        train_loss_record,train_acc_record,val_loss_record,val_acc_record=[],[],[],[]
         for epoch in range(self.num_epochs):
             self.net.train()
             metric_loss=0.0
-            metric_mse=0.0
+            metric_acc=0
             metric_total=0
-            for i,(X,y) in enumerate(self.train_iter):
+            for i,(X,y) in enumerate(train_iter):
                 X=X.to(self.device)
                 y=y.to(self.device)
                 self.optimizer.zero_grad()
@@ -53,31 +80,30 @@ class Trainer:
                 with torch.no_grad():
                     batch=X.shape[0]
                     metric_loss+=l.item()*batch
-                    metric_mse+=self.MSE(y_hat,y)
+                    metric_acc+=self.accuracy(y_hat,y)
                     metric_total+=batch
                 train_loss=metric_loss/metric_total
-                train_mse=metric_mse/metric_total
-            test_loss,test_mse=self.evaluate(self.test_iter)
-            if test_mse<self.best_score:
-                self.best_score=test_mse
-                self.best_epoch=epoch+1
+                train_acc=metric_acc/metric_total
+            val_loss,val_acc=self.evaluate(val_iter)
+            #save the best model
+            if val_acc>self.best_score:
+                self.best_score=val_acc
+                self.best_epoch=epoch
                 self.best_model_state=copy.deepcopy(self.net.state_dict())
-                
+            train_loss_record.append(train_loss)
+            train_acc_record.append(train_acc)
+            val_loss_record.append(val_loss)
+            val_acc_record.append(val_acc)
             self.history['train_loss'].append(train_loss)
-            self.history['train_mse'].append(train_mse)
-            self.history['test_loss'].append(test_loss)
-            self.history['test_mse'].append(test_mse)
-            print(f'epoch: {epoch+1},'
-                f'train_loss: {train_loss:.4f},'
-                f'train_mse: {train_mse:.4f},'
-                f'test_loss: {test_loss:.4f},'
-                f'test_mse: {test_mse:.4f}'
-            )
-
+            self.history['train_acc'].append(train_acc)
+            self.history['val_loss'].append(val_loss)
+            self.history['val_acc'].append(val_acc)
+        return train_loss_record,train_acc_record,val_loss_record,val_acc_record
+                
     def evaluate(self,data_iter):
         self.net.eval()
         metric_loss=0.0
-        metric_mse=0.0
+        metric_acc=0
         metric_total=0
         with torch.no_grad():
             for X,y in data_iter:
@@ -87,27 +113,68 @@ class Trainer:
                 l=self.loss(y_hat,y)
                 batch=X.shape[0]
                 metric_loss+=l.item()*batch
-                metric_mse+=self.MSE(y_hat,y)
+                metric_acc+=self.accuracy(y_hat,y)
                 metric_total+=batch
             test_loss=metric_loss/metric_total
-            test_mse=metric_mse/metric_total
-        return test_loss,test_mse
+            test_acc=metric_acc/metric_total
+        return test_loss,test_acc
+    
+    def get_k_fold_data(self,k,i):
+        fold_size=self.train_feature.shape[0]//k
+        X_train,y_train=None,None
+        for j in range(k):
+            if j==k-1:
+                idx=slice(j*fold_size,None)
+            else:
+                idx=slice(j*fold_size,(j+1)*fold_size)
+            if j==i:
+                X_valid,y_valid=self.train_feature[idx,:],self.train_label[idx]
+            elif X_train is None:
+                X_train,y_train=self.train_feature[idx,:],self.train_label[idx]
+            else:
+                X_train=torch.cat([X_train,self.train_feature[idx,:]],dim=0)
+                y_train=torch.cat([y_train,self.train_label[idx]],dim=0)
+        return X_train,y_train,X_valid,y_valid
+                
+    def k_fold_cross_validation(self,k):
+        train_acc_sum,val_acc_sum=0.0,0.0
+        self.best_score=0.0
+        self.best_epoch=0
+        self.best_model_state= None
+        for i in range(k):
+            self.net.load_state_dict(copy.deepcopy(self.initial_model_state))
+            self.optimizer=self.optimizer_class(self.net.parameters(),lr=self.lr,weight_decay=self.weight_decay)
+            data=self.get_k_fold_data(k,i)
+            train_loss,train_acc,val_loss,val_acc=self.train(*data)
+            train_acc_sum+=train_acc[-1]
+            val_acc_sum+=val_acc[-1]
+            print(f'fold {i + 1}, train acc {float(train_acc[-1]):.4f}, '
+                  f'validation acc {float(val_acc[-1]):f}')
+        return train_acc_sum/k,val_acc_sum/k
+    
+    def best_estimator(self):
+        if self.best_model_state is None:
+            print('No best model found')
+        else:
+            self.net.load_state_dict(copy.deepcopy(self.best_model_state))
+            self.net.to(self.device)
+        return self.net
 
     def plot(self):
-        epoch=range(1,len(self.history['train_loss'])+1)
+        epochs=range(1,len(self.history['train_loss'])+1)
         plt.figure(figsize=(8,5))
-        plt.plot(epoch,self.history['train_mse'],label='Train MSE')
-        plt.plot(epoch,self.history['test_mse'],label='Test MSE')
+        plt.plot(epochs,self.history['train_acc'],label='Train ACC')
+        plt.plot(epochs,self.history['val_acc'],label='Val ACC')
         plt.xlabel('Epochs')
-        plt.ylabel('MSE')
+        plt.ylabel('ACC')
         plt.legend()
-        plt.title('MSE vs Epochs')
+        plt.title('ACC vs Epochs')
         plt.grid(True)
         plt.show()
 
         plt.figure(figsize=(8,5))
-        plt.plot(epoch,self.history['train_loss'],label='Train Loss')
-        plt.plot(epoch,self.history['test_loss'],label='Test Loss')
+        plt.plot(epochs,self.history['train_loss'],label='Train Loss')
+        plt.plot(epochs,self.history['val_loss'],label='Val Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
@@ -115,12 +182,10 @@ class Trainer:
         plt.grid(True)
         plt.show()
 
-    def best_estimator(self):
-        if self.best_model_state is None:
-            print("No best model found.")
-            return None
-        self.net.load_state_dict(self.best_model_state)
-        print(
-            f'Best Model loaded from epoch {self.best_epoch}, '
-            f'best test MSE: {self.best_score:.4f}'
-        )
+    def predict(self,test_data):
+        net=self.best_estimator()
+        net.eval()
+        prediction=net(self.test_feature.to(self.device)).argmax(dim=1).detach().cpu().numpy()
+        test_data['PitNextLap']=pd.Series(prediction.reshape(-1))
+        submission=pd.concat([test_data['id'],test_data['PitNextLap']],axis=1)
+        submission.to_csv('submission.csv', index=False)
